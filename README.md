24cad6b03fe7ab7f527f2fc1606b1e864e99f2e5
Correnz-BlueApps <142593366+Correnz-BlueApps@users.noreply.github.com>
2025-09-25 13:42:24 +0000

    python-chefkoch

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # what-will-i-eat-this-week
 
+# run dev
 python mealplan/manage.py runserver
--- mealplan/main_app/views.py
+++ mealplan/main_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from chefkoch.recipe import Recipe
+from chefkoch.retrievers import SearchRetriever
+import json
 
 def home(request):
     return render(request, "main_app/home.html")
@@ -12,7 +15,23 @@ def about(request):
     return render(request, "main_app/about.html", context)
 
 def random(request):
+    recipe = Recipe("https://www.chefkoch.de/rezepte/3814751580479131/Pinsa.html")
+
+    retriever = SearchRetriever(
+        properties=["Einfach"],
+    )
+    recipes = retriever.get_recipes(search_query="Lasagne", page=1)
+    retriever.close()
+
+    re = json.dumps(
+        recipes[0].rating,
+        default=lambda o: o.__dict__, 
+        sort_keys=True,
+        indent=4
+    )
+
     context = {
-        "test": "test"
+        "test": re,
     }
+
     return render(request, "main_app/random.html", context)
